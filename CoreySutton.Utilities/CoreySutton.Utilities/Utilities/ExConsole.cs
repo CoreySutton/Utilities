aabb711f093d58b70ea5aac92f09e12c3a6e1ac2
Corey Sutton <coreysutton@outlook.co.nz>
2018-08-17 14:09:42 +1200

    Add ExConsole.cs

---
+++ CoreySutton.Utilities/CoreySutton.Utilities/Utilities/ExConsole.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace CoreySutton.Utilities
+{
+    /// <summary>
+    /// Wrapper for SystemConsole
+    /// </summary>
+    public static class ExConsole
+    {
+        private static ConsoleColor _defaultColor = ConsoleColor.Gray;
+
+        public static void Write(string message)
+        {
+            Console.Write(message);
+        }
+
+        public static void WriteLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void WriteLineToRight(string value)
+        {
+            if (string.IsNullOrEmpty(value)) return;
+
+            int valueLength = value.Length;
+
+            Console.CursorLeft = Console.BufferWidth - valueLength - 1;
+            Console.WriteLine(value);
+        }
+
+        public static void WriteColor(string value, ConsoleColor color)
+        {
+            if (string.IsNullOrEmpty(value)) return;
+
+            Console.ForegroundColor = color;
+            Console.Write(value);
+            Console.ForegroundColor = _defaultColor;
+        }
+
+        public static void WriteLineColor(string value, ConsoleColor color)
+        {
+            if (string.IsNullOrEmpty(value)) return;
+
+            Console.ForegroundColor = color;
+            Console.WriteLine(value);
+            Console.ForegroundColor = _defaultColor;
+        }
+    }
+}
