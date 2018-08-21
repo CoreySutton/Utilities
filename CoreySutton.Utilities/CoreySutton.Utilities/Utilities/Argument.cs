98d8a2a352427e977a5bcab5495f0da144b0aa17
Corey Sutton <coreysutton@outlook.co.nz>
2018-08-22 07:16:40 +1200

    Add some additional overloads for Argynent and Validator functions

--- CoreySutton.Utilities/CoreySutton.Utilities/Utilities/Argument.cs
+++ CoreySutton.Utilities/CoreySutton.Utilities/Utilities/Argument.cs
@@ -14,6 +14,8 @@ namespace CoreySutton.Utilities
             if (value == null) throw new ArgumentNullException(name);
         }
 
+        #region IsNullOrEmpty
+
         public static void IsNotNullOrEmpty(string value, string name = DefaultArgumentName)
         {
             if (string.IsNullOrEmpty(value)) throw new ArgumentNullException(name);
@@ -39,6 +41,25 @@ namespace CoreySutton.Utilities
             if (value == null || value.Cast<object>().Count() <= 0) throw new ArgumentNullException(name);
         }
 
+        #endregion
+
+        #region IsGreaterThanZero
+
+        public static void IsGreaterThanZero(long value, string name = DefaultArgumentName)
+        {
+            if (value <= 0L) throw new ArgumentNullException(name);
+        }
+
+        public static void IsGreaterThanZero(decimal value, string name = DefaultArgumentName)
+        {
+            if (value <= 0.0m) throw new ArgumentNullException(name);
+        }
+
+        public static void IsGreaterThanZero(float value, string name = DefaultArgumentName)
+        {
+            if (value <= 0.0f) throw new ArgumentNullException(name);
+        }
+
         public static void IsGreaterThanZero(double value, string name = DefaultArgumentName)
         {
             if (value <= 0.0) throw new ArgumentNullException(name);
@@ -49,9 +70,28 @@ namespace CoreySutton.Utilities
             if (value <= 0) throw new ArgumentNullException(name);
         }
 
+        #endregion
+
+        #region IsGreaterThanOrEqualToZero
+
+        public static void IsGreaterThanOrEqualToZero(long value, string name = DefaultArgumentName)
+        {
+            if (value < 0L) throw new ArgumentNullException(name);
+        }
+
+        public static void IsGreaterThanOrEqualToZero(decimal value, string name = DefaultArgumentName)
+        {
+            if (value < 0.0m) throw new ArgumentNullException(name);
+        }
+
+        public static void IsGreaterThanOrEqualToZero(float value, string name = DefaultArgumentName)
+        {
+            if (value < 0.0f) throw new ArgumentNullException(name);
+        }
+
         public static void IsGreaterThanOrEqualToZero(double value, string name = DefaultArgumentName)
         {
-            if (value < 0.0) throw new ArgumentNullException(name);
+            if (value < 0.0d) throw new ArgumentNullException(name);
         }
 
         public static void IsGreaterThanOrEqualToZero(int value, string name = DefaultArgumentName)
@@ -59,9 +99,28 @@ namespace CoreySutton.Utilities
             if (value < 0) throw new ArgumentNullException(name);
         }
 
+        #endregion
+
+        #region IsLessThanZero
+
+        public static void IsLessThanZero(long value, string name = DefaultArgumentName)
+        {
+            if (value >= 0L) throw new ArgumentNullException(name);
+        }
+
+        public static void IsLessThanZero(decimal value, string name = DefaultArgumentName)
+        {
+            if (value >= 0.0m) throw new ArgumentNullException(name);
+        }
+
+        public static void IsLessThanZero(float value, string name = DefaultArgumentName)
+        {
+            if (value >= 0.0f) throw new ArgumentNullException(name);
+        }
+
         public static void IsLessThanZero(double value, string name = DefaultArgumentName)
         {
-            if (value >= 0.0) throw new ArgumentNullException(name);
+            if (value >= 0.0d) throw new ArgumentNullException(name);
         }
 
         public static void IsLessThanZero(int value, string name = DefaultArgumentName)
@@ -69,14 +128,35 @@ namespace CoreySutton.Utilities
             if (value >= 0) throw new ArgumentNullException(name);
         }
 
+        #endregion
+
+        #region IsLessThanOrEqualToZero
+
+        public static void IsLessThanOrEqualToZero(long value, string name = DefaultArgumentName)
+        {
+            if (value > 0L) throw new ArgumentNullException(name);
+        }
+
+        public static void IsLessThanOrEqualToZero(decimal value, string name = DefaultArgumentName)
+        {
+            if (value > 0.0m) throw new ArgumentNullException(name);
+        }
+
+        public static void IsLessThanOrEqualToZero(float value, string name = DefaultArgumentName)
+        {
+            if (value > 0.0f) throw new ArgumentNullException(name);
+        }
+
         public static void IsLessThanOrEqualToZero(double value, string name = DefaultArgumentName)
         {
-            if (value > 0.0) throw new ArgumentNullException(name);
+            if (value > 0.0d) throw new ArgumentNullException(name);
         }
 
         public static void IsLessThanOrEqualToZero(int value, string name = DefaultArgumentName)
         {
             if (value > 0) throw new ArgumentNullException(name);
         }
+
+        #endregion
     }
 }
